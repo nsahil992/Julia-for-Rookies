84cd459965cf632bd718332ddeb78876f7720180
Sahil Naik <nsahil992@gmail.com>
2023-11-17 17:16:24 +0530

    Basic Maths Operations

--- 01Variables and Inputs/MathsOperations.jl
+++ 01Variables and Inputs/MathsOperations.jl
@@ -7,6 +7,6 @@ println(number3)
 quotient = number1/number2
 product = number1 * number2
 difference = number1 - number2
-println(quotient)
-println(product)
-println(difference)
+println("quotient : ", quotient)
+println("product : ",  product)
+println("difference : ",  difference)

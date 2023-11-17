4ece2bee30f51c92a559ff65261cbaed460483bd
Sahil Naik <nsahil992@gmail.com>
2023-11-17 15:17:56 +0530

    Basic Maths Operations

---
+++ 01Variables and Inputs/MathsOperations.jl
@@ -0,0 +1,12 @@
+# Do basic Maths operations
+
+number1 = 10
+number2 = 90
+number3 = number1 + number2
+println(number3)
+quotient = number1/number2
+product = number1 * number2
+difference = number1 - number2
+println(quotient)
+println(product)
+println(difference)

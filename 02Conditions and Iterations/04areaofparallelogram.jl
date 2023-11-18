18c2a12e5603306715b2b9fb65c4b4868b225c9c
Sahil Naik <nsahil992@gmail.com>
2023-11-18 19:12:55 +0530

    Second commit

---
+++ 02Conditions and Iterations/04areaofparallelogram.jl
@@ -0,0 +1,6 @@
+println("Enter the base of parallelogram: ")
+base = parse(Int64, readline())
+println("Enter the height of parallelogram: ")
+height = parse(Int64, readline())
+area_of_parallelogram = base * height
+println("The area of parallelogram is: ", area_of_parallelogram)

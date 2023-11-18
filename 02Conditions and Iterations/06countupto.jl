1408bb7dde9f91fb023ba53a7c756623751163ab
Sahil Naik <nsahil992@gmail.com>
2023-11-18 19:23:13 +0530

    Second commit

--- 02Conditions and Iterations/06countupto.jl
+++ 02Conditions and Iterations/06countupto.jl
@@ -1,6 +1,6 @@
 println("Enter the number to count upto: ")
 max_number = parse(Int64, readline())
 
-for iteration_num in 1:max_number
+for iteration_num in 1:max_number #start from and end at
     println(iteration_num)
 end

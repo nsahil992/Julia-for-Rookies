d45d29e095f02095bd86f799147213a6a9191d65
Sahil Naik <nsahil992@gmail.com>
2023-11-18 18:33:47 +0530

    First commit

---
+++ 01Variables and Inputs/05Index.jl
@@ -0,0 +1,7 @@
+char = 'a' # The char given is a
+next_char = 'a' + 1 # Addition of char
+char_index = convert(Int64, char)
+println("Looking at char: ", char) 
+println("The next char is: ", next_char)
+println("Char is at index: ", char_index)
+println("The index of next char is: ", convert(Int64, next_char))

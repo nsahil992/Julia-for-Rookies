f50dc9d5916eb93dc115103cc0ea733fbaa65cdd
Sahil Naik <nsahil992@gmail.com>
2023-11-19 19:22:26 +0530

    Second commit

---
+++ 02Conditions and Iterations/09userinputrange.jl
@@ -0,0 +1,9 @@
+println("Enter a number: ")
+user_input = parse(Int64, readline())
+for iteration_num in 1:100
+    if(iteration_num % user_input == 0)
+        println("-")
+    else
+        println(iteration_num)
+    end
+end

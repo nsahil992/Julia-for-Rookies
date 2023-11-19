a6bb830ced55ed167b97c161e474adbf833b1075
Sahil Naik <nsahil992@gmail.com>
2023-11-19 19:02:57 +0530

    Second commit

--- 02Conditions and Iterations/08NextorQuit.jl
+++ 02Conditions and Iterations/08NextorQuit.jl
@@ -1,10 +1,10 @@
-show_next = "c"
-next_multiple = 0
+show_next = "c" # Char c is taken to denote continue
+next_multiple = 0 # We kept next multiple as 0
 
-while show_next == "c"
-    global next_multiple = next_multiple + 1
-    println(5 * next_multiple)
-    println("Type c to continue, any other letter to stop.")
-    global show_next = readline()
+while show_next == "c" # While the user input = c
+    global next_multiple = next_multiple + 1 # Next multiple will be increased by 1
+    println(5 * next_multiple) # We will multiply next multiple by 5
+    println("Type c to continue, any other letter to stop.") 
+    global show_next = readline() # Takes user input
 end
-println("bye")
+println("bye") # If user enters any other letter, it will break the loop and it will print bye

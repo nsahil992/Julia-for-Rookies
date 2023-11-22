19ba85125348d1331c77b55f26c966fd5f054539
Sahil Naik <nsahil992@gmail.com>
2023-11-22 21:36:06 +0530

    Third commit

---
+++ 03Arrays and Dictionaries/01friendsarray.jl
@@ -0,0 +1,7 @@
+courage_age = 21
+mrbean_age = 30
+shaun_age = 25
+oswald_age = 15
+sum_of_ages = courage_age + mrbean_age + shaun_age + oswald_age
+total_friends = 4
+println("Mean of friends age: $(sum_of_ages/total_friends)")

ad33d0038b7f7cecbc62da766f91239f13028ce1
Sahil Naik <nsahil992@gmail.com>
2023-11-23 23:07:33 +0530

    Third commit

--- 03Arrays and Dictionaries/08Dictwordmeanings.jl
+++ 03Arrays and Dictionaries/08Dictwordmeanings.jl
@@ -7,5 +7,5 @@ word_meanings = Dict{String, String}("air" => "A mixture of gases that forms our
 println(word_meanings["air"])
 println(keys(word_meanings))
 println(values(word_meanings))
-word_meanings["card"] = "A rectangular piece of plastic or thick paper"
+word_meanings["card"] = "A rectangular piece of plastic or thick paper" # adds card to dictionary
 println(word_meanings)

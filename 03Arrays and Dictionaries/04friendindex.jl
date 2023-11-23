813e56aeebb3825fe544daeeb4fdec3d20c78425
Sahil Naik <nsahil992@gmail.com>
2023-11-23 15:21:08 +0530

    Third commit

---
+++ 03Arrays and Dictionaries/04friendindex.jl
@@ -0,0 +1,4 @@
+friends = ["Courage", "Mr.Bean", "Amara", "Oswald"]
+println(friends[1]) #friends[1] 1 represents the index in friends array i.e Courage
+first_friend = friends[1]
+println(first_friend)

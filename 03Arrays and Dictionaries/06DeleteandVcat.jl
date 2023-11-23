4c59bdab250e9635f6e9d8946a7133bb41d7b144
Sahil Naik <nsahil992@gmail.com>
2023-11-23 15:33:17 +0530

    Third commit

---
+++ 03Arrays and Dictionaries/06DeleteandVcat.jl
@@ -0,0 +1,6 @@
+india_team = ["Rohit", "Virat", "Rahul", "Ashwin"]
+australia_team = ["Warner", "Smith", "Maxwell", "Cummins"]
+deleteat!(australia_team, 4)
+println(australia_team)
+all_players = vcat(india_team, australia_team)
+println(all_players)

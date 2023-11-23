3b6d9151f852e81923fd286e478b1af85bc9f701
Sahil Naik <nsahil992@gmail.com>
2023-11-23 23:02:43 +0530

    Third commit

---
+++ 03Arrays and Dictionaries/08Dictwordmeanings.jl
@@ -0,0 +1,7 @@
+word_meanings = Dict{String, String}("air" => "A mixture of gases that forms our atmosphere", "food" => "substance that human beings eat for survival", "mail" => "letters or packages that are sent or recieved", "refine" => "to make something better or purer")
+# Dict tells Julia that we are going to create an Dictionary
+# String tells julia that the type of key is string and the value is also string
+# air - These are the keys of the items
+# => are the operators
+# A mixture of if the values that are associated with the keys
+println(word_meanings["air"])

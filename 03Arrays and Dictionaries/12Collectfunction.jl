6f32aad40c2df5714243821bca932979718f188b
Sahil Naik <nsahil992@gmail.com>
2023-11-26 19:18:23 +0530

    Third commit

---
+++ 03Arrays and Dictionaries/12Collectfunction.jl
@@ -0,0 +1,7 @@
+for iteration in 1:10
+    println(iteration)
+end
+
+println(collect(1:10))
+
+#collect function helps you to take that represent sequence of data and materialize those values into actual array.

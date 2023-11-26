65c3b7fb33396694c08edb5face5d4f64d63e636
Sahil Naik <nsahil992@gmail.com>
2023-11-26 19:26:40 +0530

    Fourth commit

---
+++ 04Functions/01bestfriendfunction.jl
@@ -0,0 +1,11 @@
+println("Who are your best friends? ")
+best_friends = []
+# Took an empty array
+while true
+    println("Name: ")
+    # Ask user for the best friends name
+    user_input = readline()
+    # Take user_input
+    if user_input == "done"
+        break
+    end

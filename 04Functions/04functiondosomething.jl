f10255e38d751a35e5e816f817305e250d1d5fb2
Sahil Naik <nsahil992@gmail.com>
2023-11-26 20:15:12 +0530

    Fourth commit

--- 04Functions/04functiondosomething.jl
+++ 04Functions/04functiondosomething.jl
@@ -2,5 +2,9 @@ a = 10
 
 function do_something()
     println("I did something!")
+    println("When printed inside the function a is: ")
+    println(a)
+    println("Now changing its value to 5 inside function.")
+    
     
 end

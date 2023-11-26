7872b23ece57062a6ca8323d6ac8d8d81f59d625
Sahil Naik <nsahil992@gmail.com>
2023-11-26 19:16:40 +0530

    Third commit

---
+++ 03Arrays and Dictionaries/11Joinfunction.jl
@@ -0,0 +1,7 @@
+ #join function converts array into string
+my_cartoons = ["Courage", "Mr.Bean", "Oswald", "Atashinchi"]
+my_cartoons_string = join(my_cartoons, ", ")
+println(my_cartoons_string)
+
+list_of_strings = ["a", "b", "c", "d"]
+println(join(list_of_strings, ""))

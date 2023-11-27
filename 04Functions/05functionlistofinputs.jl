46ca39132abf7e293edbeab41dea02468a66a08d
Sahil Naik <nsahil992@gmail.com>
2023-11-27 21:43:49 +0530

    Fourth commit

--- 04Functions/05functionlistofinputs.jl
+++ 04Functions/05functionlistofinputs.jl
@@ -4,5 +4,10 @@ function list_of_inputs(title::String, prompt::String)::Array{String}
     while true
         println(prompt)
         user_input = readline()
-    
+        if user_input == "done"
+            break
+        end
+        push!(input_list, user_input)
+    end
+    return input_list 
 end

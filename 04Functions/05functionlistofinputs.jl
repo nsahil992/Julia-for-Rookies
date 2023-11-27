ca0d255f96b8debb9f92f012559c654f588c9087
Sahil Naik <nsahil992@gmail.com>
2023-11-27 21:42:59 +0530

    Fourth commit

--- 04Functions/05functionlistofinputs.jl
+++ 04Functions/05functionlistofinputs.jl
@@ -1,3 +1,8 @@
 function list_of_inputs(title::String, prompt::String)::Array{String}
+    println(title)
+    input_list::Array{String} = []
+    while true
+        println(prompt)
+        user_input = readline()
     
 end

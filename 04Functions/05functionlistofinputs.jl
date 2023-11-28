21b1049661606ef9b29adfe8fc2b3407ec899c1a
Sahil Naik <nsahil992@gmail.com>
2023-11-28 19:37:04 +0530

    Fourth commit

--- 04Functions/05functionlistofinputs.jl
+++ 04Functions/05functionlistofinputs.jl
@@ -1,5 +1,8 @@
 function list_of_inputs(title::String, prompt::String)::Array{String}
+    # we created a functio list_of_inputs 
+    # we declared that title and prompt is string 
     println(title)
+    # we print the title
     input_list::Array{String} = []
     while true
         println(prompt)

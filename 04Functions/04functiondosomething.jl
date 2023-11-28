d8625f5cc0a6f7ea7d9625c49a2dcc8483b80f69
Sahil Naik <nsahil992@gmail.com>
2023-11-28 19:30:33 +0530

    Fourth commit

--- 04Functions/04functiondosomething.jl
+++ 04Functions/04functiondosomething.jl
@@ -1,9 +1,11 @@
 a = 10
-
+# we have created a variable a with a value 10
 function do_something()
+    # we created a function do_something
     println("I did something!")
     println("When printed inside the function a is: ")
     println(a)
+    #
     println("Now changing its value to 5 inside function.")
     global a = 5    
 end

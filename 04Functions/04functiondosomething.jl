6dc137c1cdfa6019ff988ec26a44dd074675ef4f
Sahil Naik <nsahil992@gmail.com>
2023-11-28 19:32:10 +0530

    Fourth commit

--- 04Functions/04functiondosomething.jl
+++ 04Functions/04functiondosomething.jl
@@ -5,10 +5,13 @@ function do_something()
     println("I did something!")
     println("When printed inside the function a is: ")
     println(a)
-    #
+    # it will print 10 when printed inside function
     println("Now changing its value to 5 inside function.")
-    global a = 5    
+    global a = 5   
+    # we changed its value to 5 
 end
 do_something()
+# we are calling the function
 println("When printed outside of function a is: ")
 println(a)
+# it will now print 5

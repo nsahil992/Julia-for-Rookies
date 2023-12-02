22ad30f5bf661f970ae559e006539ea1abd93e28
Sahil Naik <nsahil992@gmail.com>
2023-12-02 17:16:47 +0530

    Fourth commit

---
+++ 04Functions/09herons_formula2.jl
@@ -0,0 +1,5 @@
+function herons_formula_triangle_area(;side_a::Float64, side_b::Float64, side_c::Float64)::Float64
+    sp = (side_a + side_b + side_c) / 2
+    return sqrt(sp * (sp - side_a) * (sp - side_b) * (sp - side_c))
+end
+println(herons_formula_triangle_area(side_a=3.0, side_b=4.0, side_c=5.0))

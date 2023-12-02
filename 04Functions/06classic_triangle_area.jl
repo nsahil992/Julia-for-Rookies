2c022c0057ea8741330a86d5825ff07093a7fb27
Sahil Naik <nsahil992@gmail.com>
2023-12-02 17:09:46 +0530

    Fourth commit

--- 04Functions/06classic_triangle_area.jl
+++ 04Functions/06classic_triangle_area.jl
@@ -2,3 +2,8 @@ function classic_triangle_area(base::Float64, height::Float64)
     # This creates a function classic_triangle_area and declares that the base and height is of type Float64
     print("Finding the area of triangle with base = $(base), and height = $(height) units...")
     # Prints the base and height in the string
+    return (base * height) / 2 
+    # Returns the values with the formula
+end
+println(classic_triangle_area(4.0, 3.0))
+# We pass float values in the functions
